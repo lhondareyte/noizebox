8bc7d6cfa38e297749fc56058619e2f9223a7dab
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2017-10-13 20:20:55 +0200

    Exit 0 if there is no SF2 installed.

--- rsc/mksf2db.sh
+++ rsc/mksf2db.sh
@@ -1,16 +1,19 @@
 #!/bin/sh
 LOG="/tmp/$(basename $0).$$"
-FONTS="/Applications/Noizebox/Resources/SF2/*.lst"
-SF2DB="/Applications/Noizebox/Resources/soundfont.conf"
-if [ -z "$FONTS" ] ; then
+FONTSDIR="/Library/Noizebox/Resources/SF2/"
+FONTS="${FONTSDIR}/*.lst"
+SF2DB="/Library/Noizebox/Resources/soundfont.conf"
+
+ls $FONTS > /dev/null 2>&1
+if [ $? -ne 0 ] ; then
+	echo "No SF2 to register."
 	exit 0
 fi
 
-rm -f $LOG
-
 Exec() {
 	printf "$1 ..."
-	shift ; $* >> $LOG 2>&1
+	shift ; 
+	eval $* >> $LOG 2>&1
 	if [ $? -ne 0 ] ; then
 		echo "failed!"
 		exit 1
@@ -18,22 +21,21 @@ Exec() {
 	echo "done."
 }
 
-Exec "Deleting $SF2DB" rm $SF2DB
-Exec "Creating $SF2DB" sqlite3 $SF2DB "create table bank (id smallint, name varchar(12), file varchar(50) , key_offset smallint, pitch_offset smallint);"
+Exec "Deleting $SF2DB" rm -f $SF2DB
+Exec "Creating $SF2DB" sqlite3 $SF2DB \"create table bank \(id smallint, name varchar\(12\), file varchar\(50\) , key_offset smallint, pitch_offset smallint\)\;\"
 
 i=1
 for FONT in $FONTS
 do
-	printf "Building SF2 database for $FONT ... "
 	grep -v ^# $FONT | while read n f k p junk
 	do
-		if [ -f "${f}" ] ; then
-			sqlite3 $SF2DB "insert into bank values(${i}, '${n}', '${f}', '${k}', '${p}');"
+		if [ -f "${FONTSDIR}/${f}" ] ; then
+			Exec "Registering $f" sqlite3 $SF2DB \"insert into bank values\(${i}, \'${n}\', \'${f}\', \'${k}\', \'${p}\'\)\;\"
 			let i+=1 > /dev/null
 		else
-			printf "($(basename $f) does not exist, skipping) ..."
+			echo "($(basename $f) does not exist, skipping) ..."
 		fi
 	done
-	echo done
 done
+rm -f $LOG
 exit 0
