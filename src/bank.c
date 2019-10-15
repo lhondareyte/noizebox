12c04a330869c966180fa465c728c013afbf57f0
Luc Hondareyte <luc.hondareyte@laposte.net>
2019-10-15 19:36:32 +0200

    Update indentation

--- src/bank.c
+++ src/bank.c
@@ -3,14 +3,15 @@
  * 
  * All rights reserved.
  * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  * 
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright notice,
- *       this list of conditions and the following disclaimer in the documentation
- *       and/or other materials provided with the distribution.
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -33,29 +34,25 @@
 
 int max_font_in_bank;
 
-void NZ_load_bank(void)
-{
+void NZ_load_bank(void) {
 	char * sql;
 	sqlite3 *bank;
 	sqlite3_stmt *stmt;
 
 	sqlite3_initialize();
-        if ( sqlite3_open_v2(FONT_DB, &bank, SQLITE_OPEN_READONLY, NULL) != SQLITE_OK )
-        {
+        if ( sqlite3_open_v2(FONT_DB, &bank, \
+             SQLITE_OPEN_READONLY, NULL) != SQLITE_OK ) {
                 fprintf (stderr, "Error: Cannot open SF2 configuration file (%s)\n",FONT_DB);
                 exit (1);
         }
 	sql = "select count(rowid) from bank" ;
-	if ( sqlite3_prepare_v2(bank,sql,strlen(sql),&stmt,NULL) == SQLITE_OK ) 
-	{
-		if (  sqlite3_step(stmt) == SQLITE_ROW ) 
-		{
+	if ( sqlite3_prepare_v2(bank,sql,strlen(sql),&stmt,NULL) == SQLITE_OK ) {
+		if (  sqlite3_step(stmt) == SQLITE_ROW ) {
 			max_font_in_bank=sqlite3_column_int(stmt,0);
 			sqlite3_finalize(stmt);
 		}
 	}
-	else
-	{
+	else {
 		fprintf (stderr, "Error: Failed to prepare database: %s\n",sqlite3_errmsg(bank));
                 exit (1);
 	}
@@ -63,8 +60,7 @@ void NZ_load_bank(void)
 	sqlite3_shutdown();
 }
 
-void NZ_load_font(int font)
-{
+void NZ_load_font(int font) {
 	int  i;
         char sql[80];
         sqlite3 *bank;
@@ -73,30 +69,25 @@ void NZ_load_font(int font)
 	struct stat st;
 
 	/* Reset tuning for all channels */
-	for ( i=0; i<= 15; i++ )
-	{
+	for ( i=0; i<= 15; i++ ) {
 		fluid_synth_deactivate_tuning(synth, i, FALSE);
 	}
 
-	if ( current_font_id != 0 ) 
-	{
+	if ( current_font_id != 0 ) {
 		fluid_synth_sfunload(synth,current_font_id,1);
 	}
 	if (current_font == 0 ) current_font=max_font_in_bank;
 	if (current_font == max_font_in_bank + 1) current_font=1;
         
         sqlite3_initialize();
-        if ( sqlite3_open_v2(FONT_DB, &bank, SQLITE_OPEN_READONLY, NULL) != SQLITE_OK )
-        {
+        if ( sqlite3_open_v2(FONT_DB, &bank, SQLITE_OPEN_READONLY, NULL) != SQLITE_OK ) {
                 fprintf (stderr, "Error: Cannot open SF2 configuration file\n");
                 exit (1);
         }
 	
 	sprintf (sql, "select name, file, key_offset from bank where rowid='%d'", current_font);
-        if ( sqlite3_prepare_v2(bank,sql,strlen(sql),&stmt,NULL) == SQLITE_OK )
-        {
-		if ( sqlite3_step(stmt) == SQLITE_ROW ) 
-		{
+        if ( sqlite3_prepare_v2(bank,sql,strlen(sql),&stmt,NULL) == SQLITE_OK ) {
+		if ( sqlite3_step(stmt) == SQLITE_ROW ) {
 			sprintf(current_font_name,"%s",sqlite3_column_text(stmt,0));
 			sprintf(current_font_path,"%s/Resources/SF2/%s",NZDIR,sqlite3_column_text(stmt,1));
 			sprintf(ramdisk_font_path,"/Ramdisk/SF2/%s",sqlite3_column_text(stmt,1));
@@ -105,8 +96,7 @@ void NZ_load_font(int font)
 		}
 		
         }
-        else
-        {
+        else {
                 fprintf (stderr, "Error: Failed to prepare database: %s\n",sqlite3_errmsg(bank));
                 exit (1);
         }
@@ -130,8 +120,7 @@ void NZ_load_font(int font)
 	}
 
 	/* turn on tuning for all channels */
-	for ( i=0; i<= 15; i++)
-	{
+	for ( i=0; i<= 15; i++) {
 		fluid_synth_activate_tuning(synth, i, 1, 1, FALSE);
 	}
 	/* Restore current tuning */
