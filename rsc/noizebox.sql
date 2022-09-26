255ad89e9574822d2056c6c9bbe81c011634ea71
Luc Hondareyte <lhondareyte@users.noreply.github.com>
2022-09-26 19:33:11 +0200

    Update copyright

--- rsc/noizebox.sql
+++ rsc/noizebox.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c)2016,  Luc Hondareyte 
+-- Copyright (c)2016-2022,  Luc Hondareyte 
 -- All rights reserved.
 --
 -- Redistribution and use in source and binary forms, with or without
